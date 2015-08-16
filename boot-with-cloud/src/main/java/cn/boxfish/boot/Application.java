b68df7fbb67ade3f0c63f64095d55f1ec2e945fd
undancer <undancer@gmail.com>
2015-08-16 20:33:57 +0800

    提交新的sample

--- boot-with-cloud/src/main/java/cn/boxfish/boot/Application.java
+++ boot-with-cloud/src/main/java/cn/boxfish/boot/Application.java
@@ -1,12 +1,20 @@
 package cn.boxfish.boot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.cloud.bootstrap.BootstrapConfiguration;
+import org.springframework.context.annotation.ImportResource;
 
 /**
  * Created by undancer on 15/6/20.
  */
-public class Application {
+@SpringBootApplication(exclude = {})
+@BootstrapConfiguration
+@ImportResource("classpath*:META-INF/spring.xml")
+public class Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
-        System.exit(SpringApplication.exit(SpringApplication.run(Application.class, args)));
+        SpringApplication.run(Application.class, args);
+//        System.exit(SpringApplication.exit(SpringApplication.run(Application.class, args)));
     }
 }
---
+++ boot-with-cloud/src/main/java/cn/boxfish/boot/cloud/ConfigServer.java
@@ -0,0 +1,8 @@
+package cn.boxfish.boot.cloud;
+
+/**
+ * Created by undancer on 15/6/20.
+ */
+//@EnableConfigServer
+public class ConfigServer {
+}
